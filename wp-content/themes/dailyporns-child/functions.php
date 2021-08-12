971904f1a312babb6a48090459b58e49762a95db
Mr-hiddenaire <Mr-hiddenaire@Mr-hiddenaire.com>
2021-08-12 23:53:11 +0800

    done.

--- wp-content/themes/dailyporns-child/functions.php
+++ wp-content/themes/dailyporns-child/functions.php
@@ -89,6 +89,16 @@ function setAWSCookie()
     }
 }
 
+function block_wp_admin_init()
+{
+    if (strpos(strtolower($_SERVER['REQUEST_URI']),'/wp-admin/') !== false) {
+        if (!is_super_admin()) {
+            wp_redirect(get_option('siteurl'), 302);
+        }
+    }
+}
+
+add_action('init','block_wp_admin_init',0);
 add_action('wp_enqueue_scripts', 'dailyporns_child_scripts', 11);
 add_filter('manage_posts_columns', 'gt_posts_column_views');
 add_action('manage_posts_custom_column', 'gt_posts_custom_column_views');
